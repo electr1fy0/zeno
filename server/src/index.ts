4695a9ea62f45766fa81e3ca3d34871a4ded3b57
electr1fy0 <electr1fy0@icloud.com>
2026-03-19 18:23:53 +0530

    feat: implement multiple chats on server

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,24 +14,22 @@ const model = google("gemini-3.1-flash-lite-preview");
 // });
 
 interface Chat {
+  id: number;
   messages: ModelMessage[];
 }
 
-let chat: Chat = {
-  messages: [
-    {
-      role: "system",
-      content: "dont use any markdown formatting in your output",
-    },
-  ],
-};
+let chats: Record<number, Chat> = {};
 
-async function getResponse(msg: string): Promise<string> {
+async function getResponse(chatId: number, msg: string): Promise<string> {
+  let chat: Chat = chats[chatId];
+  console.log(chat);
   chat.messages.push({ role: "user", content: msg });
+
   const { text, reasoning } = await generateText({
     model: model,
     messages: chat.messages,
   });
+
   chat.messages.push({ role: "assistant", content: text });
 
   return text;
@@ -54,10 +52,33 @@ app.post("/chat/:id", async (req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
   res.set("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.set("Access-Control-Allow-Headers", "Content-Type");
+  const id = parseInt(req.params.id);
+
   const { message } = req.body;
-  console.log(message);
-  const aiResp = await getResponse(message);
+  console.log(message, id);
+  console.log("chat: ", chats[id]);
+  const aiResp = await getResponse(id, message);
   res.send(aiResp);
+});
+
+app.get("/chat", (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
+
+  let id = Math.floor(Math.random() * 100);
+  console.log("id:", id);
+  chats[id] = {
+    id: id,
+    messages: [
+      {
+        role: "system",
+        content: "dont use any markdown formatting in your output",
+      },
+    ],
+  };
+  res.send(id);
+  console.log(chats);
 });
 
 app.listen(3000, () => console.log("running..."));
